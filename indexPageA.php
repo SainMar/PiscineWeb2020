ddb2164ad2620a0c19f62b8f0d1b2b112af16967
Arthur Saint Martin <saintmartinarthur@gmail.com>
2020-04-12 18:38:21 +0200

    test, troiseimepassage

--- indexPageA.php
+++ indexPageA.php
@@ -12,7 +12,7 @@
         <link rel="stylesheet" type="text/css" href="style.css">
        
     </head>
-    <body>
+    <body style="background-color:red;">
     <form action="3Traitement.php" method="POST">
      
         <label for="id">ID--11</label><input type="number" name="id" id="id"><br>
