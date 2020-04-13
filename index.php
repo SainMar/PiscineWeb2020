9113bc1b3ff38d16af5ba815d19dad09230e6c12
Arthur Saint Martin <saintmartinarthur@gmail.com>
2020-04-13 17:15:10 +0200

    template bootstrap, base pages acheteurs vendeur et admin

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>ECEbay</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+        
+        <link rel="stylesheet" type="text/css" href="style.css">
+       
+    </head>
+    <body>
+        <div class="container-fluid" id="top-content">
+            
+            <div class="row" id="top-top-content">
+                <div class="col-2">
+                    <!--<h1 class="display-4" id="logo">ECEbay</h1>-->
+                </div>
+                <div class="col-7">
+
+                </div>
+                <div class="col-3">
+                   <div class="row">
+                        <!--
+                        <div class="col-6">
+                            <p>Connect to account</p>
+                        </div>
+                        <div class="col-6">
+                            <button type="button" class="btn btn-outline-light">Sign in</button>
+                        </div>-->
+
+                        
+                    </div> 
+                   
+                </div>
+
+            </div>
+
+            <div class="row" id="mid-top-content">
+                <div class="col-6">
+                <div class=>
+                    <h1 class="display-4">Welcome to ECEbay</h1>
+                    <p class="lead">This is a simple webmarket, where you can find anything !</p>
+                    <hr class="my-4">
+                    <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                    <p class="lead">
+                        <a class="btn btn-primary btn-lg" href="#" role="button">Sign Up</a>
+                    </p>
+                    </div>
+                </div>
+                <div class="col-3">
+                
+                </div>
+                <div class="col-3">
+                    
+                </div>
+
+            </div>
+            <div class="row" id="bot-top-content">
+                    <div class="col-4" id="row-bot-top-content">
+
+                    </div>
+            </div>
+
+
+
+
+
+        </div>
+
+
+
+
+
+    </body>
+
+
+
+</html>
